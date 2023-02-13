a9b36c7a84265d7df2ec7b66ca1af635462dc16a
Charles GIBAULT <Charles.GIBAULT@consulting-for.edenred.com>
2023-02-13 16:56:13 +0000

    Merged PR 6174: feat(commitlint): LTT-4062 jira ticket number not accepted in commit message
    feat(commitlint): LTT-4062 jira ticket number not accepted in commit message

--- commitLint/.commitlintrc.js
+++ commitLint/.commitlintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-};  
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'subject-case': [0], // level: disabled
+    'function-rules/subject-case': [
+      2, // level: error
+      'always',
+      (parsed) => {
+        const jiraTypes = ['fix','feat'];
+        const basicRegexp = '^[a-z]';
+        const jiraRegexp = '^([A-Z]+-[0-9]+ [a-z]|[a-z].+#[A-Z]+-[0-9]+$)';
+
+        return parsed.subject?.match(jiraTypes.includes(parsed.type) ? jiraRegexp : basicRegexp)
+          ? [true]
+          : [false, `subject must contain JIRA ticket number and start with lowercase'. Eg. 'LTT-1234 sample message' or 'sample message #LTT-1234'`];
+      },
+    ],
+  },
+};
